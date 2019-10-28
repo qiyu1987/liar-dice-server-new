799dcad30d320f1d6562d0c5eaf743cb2b4e5dd3
qiyu1987 <qiyu1987@gmail.com>
2019-10-28 11:01:54 +0100

    connect database

---
+++ db.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize')
+const databaseURL = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+const db = new Sequelize(databaseURL)
+db.sync()
+    .then(()=>console.log('Database connected'))
+    .catch(error => console.error)
+
+module.exports = db
